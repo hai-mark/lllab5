2ff61ccfb7b98df89de4f09f8b0631fa869cd868
hai-mark <errrorfour@gmail.com>
2024-11-23 04:17:19 +0700

    добавлен код для mainactivity и активность ResultActivity

--- app/src/main/java/com/example/lllab5/MainActivity.kt
+++ app/src/main/java/com/example/lllab5/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.lllab5
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,24 +14,30 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    public lateinit var checkbox1: CheckBox
-    public lateinit var checkbox2: CheckBox
-    public lateinit var checkbox3: CheckBox
-    public lateinit var count: EditText
-    public lateinit var button: Button
-
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val pagesEditText = findViewById<EditText>(R.id.pagesEditText)
+        val formatRadioGroup = findViewById<RadioGroup>(R.id.formatRadioGroup)
+        val calculateButton = findViewById<Button>(R.id.calculateButton)
+
+        calculateButton.setOnClickListener {
+            val pages = pagesEditText.text.toString().toIntOrNull() ?: 0
+            val selectedId = formatRadioGroup.checkedRadioButtonId
+            val radioButton = findViewById<RadioButton>(selectedId)
+            val pricePerPage = when (radioButton?.text) {
+                "А4 (10 руб.)" -> 10
+                "А3 (30 руб.)" -> 30
+                "А1 (100 руб.)" -> 100
+                else -> 0 // Default case, should not happen
+            }
+            val total = pages * pricePerPage
+
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("total", total)
+            startActivity(intent)
         }
     }
 }
