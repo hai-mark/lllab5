1399980dfcfad5e294324d1ca3a68c0ed267744c
hai-mark <errrorfour@gmail.com>
2024-11-23 04:21:00 +0700

    добавлен код для ResultActivity

--- app/src/main/java/com/example/lllab5/ResultActivity.kt
+++ app/src/main/java/com/example/lllab5/ResultActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lllab5
 
 import android.os.Bundle
+
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,11 @@ import androidx.core.view.WindowInsetsCompat
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_result)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val total = intent.getIntExtra("total", 0)
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        resultTextView.text = "Сумма заказа = $total руб."
+
     }
 }
