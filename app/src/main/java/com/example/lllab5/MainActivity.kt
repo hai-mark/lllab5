d4ef122ddf63c63a6822e39740e2a9d7a83722fe
hai-mark <errrorfour@gmail.com>
2024-11-23 04:15:06 +0700

    добавлены элементы инферфейса

--- app/src/main/java/com/example/lllab5/MainActivity.kt
+++ app/src/main/java/com/example/lllab5/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.lllab5
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    public lateinit var checkbox1: CheckBox
+    public lateinit var checkbox2: CheckBox
+    public lateinit var checkbox3: CheckBox
+    public lateinit var count: EditText
+    public lateinit var button: Button
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
